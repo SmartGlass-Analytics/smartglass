c848f4696249ecbdd3bf48bd5dfa89934b34993b
johna <jacarroll4@crimson.ua.edu>
2025-02-14 14:43:26 -0600

    Added 5 buttons, set view to show those 5 buttons. No mapped functionality yet but buttons are on the screen

--- app/src/main/java/com/example/smartglassapplication/MainActivity.kt
+++ app/src/main/java/com/example/smartglassapplication/MainActivity.kt
@@ -2,11 +2,8 @@ package com.example.smartglassapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,22 +12,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartglassapplication.ui.theme.SmartglassApplicationTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            SmartglassApplicationTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
@@ -39,7 +26,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color=Color.Red) {
         Text(
             text = "Hello $name!",
-            modifier = modifier.padding(0.dp)
+            modifier = modifier.padding(24.dp)
         )
     }
 }
