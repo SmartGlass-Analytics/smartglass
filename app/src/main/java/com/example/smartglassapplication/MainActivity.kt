e80a2c43be8ffd8057abda0a48ec2effd07c93f6
johna <jacarroll4@crimson.ua.edu>
2025-02-14 17:48:13 -0600

    Added listeners to player specific buttons

--- app/src/main/java/com/example/smartglassapplication/MainActivity.kt
+++ app/src/main/java/com/example/smartglassapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartglassapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
@@ -18,6 +20,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener{ pointGuardStats(this)}
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener{ shootingGuardStats(this)}
+
+        val button3: Button = findViewById(R.id.button3)
+        button3.setOnClickListener{ smallForwardStats(this)}
+
+        val button4: Button = findViewById(R.id.button4)
+        button4.setOnClickListener{ powerForwardStats(this)}
+
+        val button5: Button = findViewById(R.id.button5)
+        button5.setOnClickListener{ centerStats(this)}
+
     }
 }
 
@@ -29,6 +46,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier.padding(24.dp)
         )
     }
+}
+
+fun pointGuardStats(view: MainActivity){
+    Toast.makeText(view, "5 Pts, 4 Ast, 1 Reb", Toast.LENGTH_SHORT).show()
+}
+
+fun shootingGuardStats(view: MainActivity){
+    Toast.makeText(view, "10 Pts, 1 Ast, 2 Reb", Toast.LENGTH_SHORT).show()
+}
+
+fun smallForwardStats(view: MainActivity){
+    Toast.makeText(view, "8 Pts, 0 Ast, 4 Reb", Toast.LENGTH_SHORT).show()
+}
+
+fun powerForwardStats(view: MainActivity){
+    Toast.makeText(view, "2 Pts, 0 Ast, 6 Reb", Toast.LENGTH_SHORT).show()
+}
+
+fun centerStats(view: MainActivity){
+    Toast.makeText(view, "3 Pts, 1 Ast, 11 Reb", Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
