70f89604865679ccb18c9e2ea952284466b82fbc
Aiden Carroll <jacarroll4@crimson.ua.edu>
2025-04-03 11:32:42 -0500

    adding stats to created player json and updating text file for box score to reflect json changes

--- apipractice.py
+++ apipractice.py
@@ -23,7 +23,7 @@ def get_access_token(url, client_id, client_secret):
     )
     token = response.json()["access_token"] # token itself
     duration = response.json()["expires_in"] # saving when token expires for reminders later
-
+    
     # write to a text file and store our newly created token
     with open(TOKEN_FILE, "w") as file:
         file.write(f"Warning: Token expires in {duration} seconds.")
@@ -37,22 +37,76 @@ def printPlayerStats(boxJS):
         teamOne = boxJS[0]["data"]["team"]["fullName"]
         switchedTeam = False
         file.write(teamOne + "\n")
+        playerstats = {}
         for entry in boxJS:
-            data = entry["data"]
+            data = entry["data"] # obtain relevant info from json
+
+            # Logic for knowing when to switch printed team
             if data["team"]["fullName"] != teamOne and not switchedTeam:
                 file.write("\n" + data["team"]["fullName"] + "\n")
                 switchedTeam = True
-            playerName = json.dumps(data["player"]["name"]).strip('"')
-            points = json.dumps(data["points"])
-            oreb = json.dumps(data["offReb"])
-            dreb = json.dumps(data["defReb"])
-            assists = json.dumps(data["assists"])
-            resultString = f"{playerName}: {points} PTS, {assists} AST, {oreb} OREB, {dreb} DREB"
-            if "ppp" in data.keys(): 
-                ppp = json.dumps(data["ppp"]) 
-                resultString += f", {ppp[0:5]} PPP"
-            file.write(resultString +"\n")
 
+            # Stat retreival from Server json
+            playerName = json.dumps(data["player"]["name"]).strip('"')
+            statString = f"{playerName}: "
+            pjson = {}
+
+            points = json.dumps(data["points"])
+            statString += f"{points} PTS, "
+            pjson['points'] = points
+
+            assists = json.dumps(data["assists"])
+            statString += f"{assists} AST, "
+            pjson['assists'] = assists
+
+            astTurnRatio = json.dumps(data["assistsTurnover"])
+            statString += f"{astTurnRatio} AST/TO, "
+            pjson['AST/TO'] = astTurnRatio
+
+            oreb = json.dumps(data["offReb"])
+            statString += f"{oreb} OREB, "
+            pjson['oreb'] = oreb
+
+            dreb = json.dumps(data["defReb"])
+            statString += f"{dreb} DREB, "
+            pjson['dreb'] = dreb
+
+            if "ppp" in data:
+                ppp = json.dumps(data["ppp"])[:4]
+            else:
+                ppp = "n/a"
+            statString += f"{ppp} PPP, "
+            pjson['ppp'] = ppp
+
+            ftMade = json.dumps(data["ftMade"])
+            ftTotal = json.dumps(data["ftAttempt"])
+            statString += f"{ftMade}/{ftTotal} FT, "
+            pjson['ftMade'] = ftMade
+            pjson['ftTotal'] = ftTotal
+
+            threePointMade = json.dumps(data["shot3Made"])
+            threePointTotal = json.dumps(data["shot3Attempt"])
+            statString += f"{threePointMade}/{threePointTotal} 3PT, "
+            pjson['3ptMade'] = threePointMade
+            pjson['3ptTotal'] = threePointTotal
+
+            shotsMade = json.dumps(data["fgMade"])
+            shotsTotal = json.dumps(data["fgAttempt"])
+            statString += f"{shotsMade}/{shotsTotal} FG, "
+            pjson['fgMade'] = shotsMade
+            pjson['fgTotal'] = shotsTotal
+
+            time = int(json.dumps(data["secondsPlayed"])) // 60
+            statString += f"{time} MIN"
+            pjson['min'] = time
+        
+            # Text File and Json Creation
+            file.write(statString +"\n")
+            playerstats[playerName] = pjson
+
+        with open('result.json', 'w') as fp:
+            json.dump(playerstats, fp, indent=4)
+        
 
 def retreive_game_stat(game_id, access_token):
     url = f"http://basketball.synergysportstech.com/external/api/games/{game_id}/boxscores"
